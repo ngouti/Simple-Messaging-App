c43a9007de5e26551e994b4a0bd5b81e9cc46f25
Emma Gouti <gouti.naama@gmail.com>
2019-04-08 13:03:18 -0500

    added socket.io; initalized socket in frontend; set up http server; set up IO connection

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
+// setting up regular HTTP server with Node that will then share with Express and Socket.IO
+var http = require('http').Server(app)
+// sets up http server
+var io = require('socket.io')(http)
+// passes in reference to http; this sets up socket for backend. Now set up in front end
+
 app.use(express.static(__dirname))
 // serves to client the index, __dirname serves the entire directory
 
@@ -9,6 +15,7 @@ app.use(bodyParser.json())
 // adding middleware; .json() lets body-parser know we expect json coming in with HTTP request
 
 app.use(bodyParser.urlencoded({extended: false}))
+// input from browser is URL encoded
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
@@ -28,7 +35,10 @@ app.post('/messages', (req, res) =>{
 // in order to make a post request, use POSTMAN. Express doesn't have a body parser so use
 // npm i -d body-parser to parse the post request
 
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
 
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
