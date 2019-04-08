4ed1a956211eeb64249fb3deaf99dfd42e402bc0
Emma Gouti <gouti.naama@gmail.com>
2019-04-08 12:03:03 -0500

    added express body-parser & post request handler

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
@@ -13,6 +14,14 @@ app.get('/messages', (req, res) =>{
     res.send(messages)
 })
 // creates path localhost:3000/messages where variable message value can be seen
+
+app.post('/messages', (req, res) =>{
+    console.log(req.body)
+    res.sendStatus(200)
+})
+// in order to make a post request, use POSTMAN. Express doesn't have a body parser so use
+// npm i -d body-parser to parse the post request
+
 
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
